0a501c557122a2021cbf7203821483c447cd6493
Alvin Alamsyah <32268037+vinalamsyah@users.noreply.github.com>
2017-09-26 03:11:26 +0700

    edit
    edit konstanta

--- singchan.c
+++ singchan.c
@@ -2,11 +2,18 @@
 #include<stdlib.h>
 #include<math.h>
 
-#define g 9.8
-#define b 3.5
+#define g 9.8 ///gravitational acceleration
+#define b 3.5 ///channel width
+#define nm 0.015 ///manning coefficient
+#define Q 4 ///discharge
+#define m 1.5
+#define s 0.001
+#define dx 1.
+#define H0 1.
 
 int main()
 {
+    double h[100];
     printf("hello world\n");
 
     return 0;
