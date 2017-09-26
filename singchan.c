2e55283963f31904cf18216626b5109f74c8393b
Alvin Alamsyah <32268037+vinalamsyah@users.noreply.github.com>
2017-09-26 10:45:12 +0700

    edit
    edit lagi

--- singchan.c
+++ singchan.c
@@ -8,12 +8,16 @@
 #define Q 4 ///discharge
 #define m 1.5
 #define s 0.001
-#define dx 1.
-#define H0 1.
+#define H0 1. ///water level at x=0
+#define L 2000. ///channel length
+#define Nx 2000 ///number of discrete point
+
+double h[Nx+1];
+double dx;
 
 int main()
 {
-    double h[100];
+    dx=L/Nx;
     printf("hello world\n");
 
     return 0;
